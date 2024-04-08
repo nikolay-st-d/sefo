0f6f23a5a4381c796a15bb458128d986197c8e08
Nikolay St. Dimitrov <126271396+nikolay-st-d@users.noreply.github.com>
2024-04-08 19:25:20 +0300

    footer link edit

--- footer_bg.php
+++ footer_bg.php
@@ -2,6 +2,6 @@
     Всички права запазени!<br>
     &copy;
     <?php echo date('Y'); ?> Сейфетин Шекеров - Сефо<br>
-    <div class="coding_copy"><a target="_blank" href="https://www.linoart.com" title="Linoart studio">Custom
+    <div class="coding_copy"><a target="_blank" href="https://www.linoart.com/index-bg.php" title="Програмиране и дизайн на уникални уеб-сайтове">Custom
             Coding: N. Dimitrov, Linoart Ltd.</a></div>
 </footer>
